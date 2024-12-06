db4340f312a355c7e19304c717ee58d85ac0712c
Yago Henrique <yago.gif@gmail.com>
2024-12-06 00:22:57 -0300

    5-1

--- 2024/5-1/5-1.rs
+++ 2024/5-1/5-1.rs
@@ -2,16 +2,53 @@ use std::{collections::HashMap, fs};
 
 fn main() {
     let file = fs::read_to_string("/home/yago/w/rust/advent-of-code/2024/5-1/input.txt").expect("");
-    let lines = file.lines();
+    let lines: Vec<&str> = file.lines().collect();
 
-    let map: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut map: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut split_index = 0;
 
-    for line in lines {
-        for num in line.split('|') {
-            map.0 = Vec::new();
-        }
-        if line == "" {
+    for (index, line) in lines.iter().enumerate() {
+        if *line == "" {
+            split_index = index;
             break;
         }
+
+        let mut values = line.split('|');
+
+        let x = values.next().unwrap();
+        let y = values.next().unwrap();
+
+        map.entry(x)
+            .and_modify(|values| values.push(y))
+            .or_insert(vec![y]);
     }
+
+    let mut result = 0;
+
+    let lines: Vec<&str> = lines.iter().skip(split_index + 1).copied().collect();
+
+    'line_for: for line in lines.iter() {
+        let line = line.split(',').rev();
+        let array: Vec<&str> = line.clone().collect();
+
+        'number_for: for (index, number) in line.enumerate() {
+            match map.get(number) {
+                None => continue 'number_for,
+                Some(number_vec) => {
+                    for i in index + 1..array.len() {
+                        if number_vec.contains(&array[i]) {
+                            continue 'line_for;
+                        }
+                    }
+                }
+            }
+        }
+
+        let num_index = ((array.len() - 1) / 2) + 1;
+        let num: usize = array[num_index - 1].parse().unwrap();
+
+        result += num;
+    }
+
+    println!("{}", result);
 }
