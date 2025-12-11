3c93f92a439b82119e6f2cd78a94ed64d13da326
Yago Henrique <yago.gif@gmail.com>
2025-12-10 21:32:13 -0300

    7-2

--- 2025/07/2.sh
+++ 2025/07/2.sh
@@ -1,43 +1,51 @@
-split(){
-    local pos=$1
-    local line_i=$2
-    local n=$3
-    local next_line=${lines[$line_i+1]}
-
-    if (( line_i < len - 3 )); then
-
-        local next_i=$(( $line_i + 2))
-        local left_pos=$(( $pos - 1))
-        local right_pos=$(( $pos + 1))
-
-        split $next_i $left_pos $(( n+1 ))
-        split $next_i $right_pos $(( n+1 ))
-    else
-        count=$((count+n))
-    fi
-
-else
-    if (( line_i < len - 3 )); then
-        local next_i=$(( $line_i + 2))
-        split $next_i $pos $n
-    else
-        count=$((count+n))
-        if [[ ${next_line:pos:1} == "^" ]];then
-        fi
-
-    fi
-}
-
-
-# puzzle_file="./puzzle.txt"
-puzzle_file="./example.txt"
+puzzle_file="./puzzle.txt"
+# puzzle_file="./example.txt"
 # puzzle_file="./dev.txt"
 
 readarray -t lines < $puzzle_file
-lines[1]=${lines[0]//S/|}
 
-len=${#lines[@]}
+pipes=()
+
+column_len=${#lines[0]}
+
+for ((i = 0; i < column_len; i++)); do
+    pipes+=(0)
+done
+
+first_pipe=${lines[0]%%S*}
+first_pipe_index=${#first_pipe}
+pipes[$first_pipe_index]=1
+
+for line_i in ${!lines[@]}; do
+    line=${lines[$line_i]}
+
+    if (( line_i % 2 == 0)); then
+        # echo $line
+
+        i=0
+        while IFS= read -n1 char; do
+            if [[ $char == "^" ]]; then
+                previous=$(( $i - 1 ))
+                next=$(( $i + 1 ))
+
+                pipes[$next]=$(( pipes[next] + pipes[i] ))
+                pipes[$previous]=$(( pipes[previous] + pipes[i] ))
+
+                pipes[$i]=0
+            fi
+
+            (( i++ ))
+        done <<< "$line"
+    fi
+done
+
 count=0
-split 7 1 1
+str=""
+for ((i = 0; i < column_len; i++)); do
+    value=${pipes[$i]}
+    count=$(( count + value ))
+    str+="${value} "
+done
 
+# echo $str
 echo $count
